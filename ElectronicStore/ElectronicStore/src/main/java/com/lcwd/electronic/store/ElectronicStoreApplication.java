8c5736586bbebc78412ebb833ec29dd241985ccc
Abhay Tomar <abhaytomar2024@gmail.com>
2024-03-04 15:21:29 +0530

    Initial commit

---
+++ ElectronicStore/ElectronicStore/src/main/java/com/lcwd/electronic/store/ElectronicStoreApplication.java
@@ -0,0 +1,14 @@
+package com.lcwd.electronic.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication
+@EnableWebMvc
+public class ElectronicStoreApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ElectronicStoreApplication.class, args);
+	}
+
+}
